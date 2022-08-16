225b43cd5793e293064c052e582aaf1b91ab5e38
my.h.m@yandex.ru <Zxcv7788jetbrains>
2022-08-16 12:33:48 +0300

    Initial commit

---
+++ src/main/java/pro/sky/java/course2/employeebookcommonslang3/EmployeeBookCommonsLang3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeebookcommonslang3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookCommonsLang3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeBookCommonsLang3Application.class, args);
+	}
+
+}
---
+++ src/test/java/pro/sky/java/course2/employeebookcommonslang3/EmployeeBookCommonsLang3ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.java.course2.employeebookcommonslang3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookCommonsLang3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
